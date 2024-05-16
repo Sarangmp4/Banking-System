1f70bca45eff5b58727d3d8d8471f65f9ce9bcab
sarang mp <mpsarang8@gmail.com>
2024-05-16 09:34:25 +0530

    Class Creation

---
+++ banking_system.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+class Account
+{
+  string Account_number;
+  string User_name;
+  double Balance;
+};
+class  Transaction
+{
+  string Transaction_Id;
+  double Amount;
+};
+class User
+{
+  string UserId;
+  string Password; 
+};
+class Admin
+{
+  void ViewAll(void)
+  {
+
+  }
+  void DeleteAccount(string Acccount_number)
+  {
+
+  }
+};
